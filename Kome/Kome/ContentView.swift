f6f587189c8a60715d54ce48856200596d656585
Nicole Milmine <122618341+n2milmin@users.noreply.github.com>
2025-03-18 09:02:44 -0400

    Kome creation

---
+++ Kome/Kome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kome
+//
+//  Created by Nicole Milmine on 2025-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to Kome!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kome/Kome/KomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomeApp.swift
+//  Kome
+//
+//  Created by Nicole Milmine on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct KomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
